104e5150a2dbf66925da5a702ef93e2f1008ea8d
Tyler <tyler.landivar@gmail.com>
2025-01-02 18:46:09 -0500

    add mobile navigation

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -2,34 +2,39 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
-import { Router } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { PersonalUserInfoService } from "../global-services/personalUserinfo.service";
+
 
 @Component({
     selector: 'navbar',
     standalone: true,
-    imports: [MatIconModule, MatMenuModule, CommonModule],
+    imports: [MatIconModule, MatMenuModule, CommonModule, RouterLink],
     templateUrl: './navbar.component.html'
   })
   export class NavBar {
     pfp: string;
-
+    sidenavOpen:boolean = false;
+    userMenuOpen:boolean = false;
+    onMobile: boolean = window.innerWidth < 768;
+    
     constructor(private router: Router, private personalUserInfoService: PersonalUserInfoService){   
         this.personalUserInfoService.userInfo.subscribe(newInfo => {
             this.pfp = newInfo.pfp
-          })
+        })
     }
 
     navigateTo(routeName: string){
+        this.sidenavOpen = false;
         this.router.navigate(['/' + routeName]);
       }
     
-      logout(){
+    logout(){
         this.personalUserInfoService.logoutUser().subscribe(loggedOut => {
           this.personalUserInfoService.updateLoggedIn(false)
           localStorage.clear()
           this.router.navigate(['/login'])
         })
-      }
+    }
 
   }
